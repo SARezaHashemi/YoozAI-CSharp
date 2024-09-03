cafd7338e20b573e74a2bdc7d7f2f5b032c4405f
SARezaHashemi <samhashemi87@gmail.com>
2024-09-03 15:58:10 +0330

    read pattern from a text file

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,7 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using YoozAI;
 
-string pattern = @"
+//this is a sample pattern
+/*string pattern = @"
     )
     + سبحان الله همش گوشته
     - سبحان الله همینطوره، این نسخه تقدیم میشه به تمام نال ها و آقای اجاقی
@@ -10,10 +11,17 @@ string pattern = @"
     + سلام من نال هستم
     - سلام وقتت بخیر نال عزیز
     (
-    ";
+    ";*/
 
-string ask = "سلام من نال هستم";
-using (YoozHandler handler = new(pattern))
+
+string filePattern = "";
+string patternPath = "pattern.txt";
+using(StreamReader reader = new (patternPath))
+{
+    filePattern = reader.ReadToEnd();
+}
+string ask = "سبحان الله همش گوشته";
+using (YoozHandler handler = new(filePattern))
 {
     //as you know windows terminal doesn't support persian texts 
     //so i prefer to save text in a file
@@ -22,10 +30,7 @@ using (YoozHandler handler = new(pattern))
     if (File.Exists(path))
     {
         File.Delete(path);
-        File.Create("test.txt");
     }
-    using (StreamWriter sw = new StreamWriter(path))
-    {
-        sw.WriteLine(handler.GetResponse(ask));
-    }
+    using StreamWriter sw = new(path);
+    sw.WriteLine(handler.GetResponse(ask));
 }
